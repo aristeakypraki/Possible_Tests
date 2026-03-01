f525f93db19772407c8cad85f3ddf3a5fe833435
aristea kypraki <aristea.kypraki@novidea.gr>
2026-03-01 15:00:07 +0200

    fix local variable loginscreen

--- src/test/java/tests/SettingsTests.java
+++ src/test/java/tests/SettingsTests.java
@@ -3,7 +3,6 @@ package tests;
 import helpers.BaseTest;
 import helpers.DriverFactory;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import screens.LoginScreen;
@@ -14,7 +13,6 @@ import java.util.logging.Logger;
 
 import java.net.MalformedURLException;
 
-import static org.testng.Assert.*;
 
 public class SettingsTests extends BaseTest {
     private static final Logger logger = Logger.getLogger(SettingsTests.class.getName());
@@ -35,7 +33,7 @@ public class SettingsTests extends BaseTest {
             driver = DriverFactory.getDriver(); // παίρνουμε τον ίδιο driver
         }
         settingsScreen = new SettingsScreen(driver);
-        loginScreen = new LoginScreen(driver);
+        LoginScreen loginScreen = new LoginScreen(driver);
         loginScreen.waitForMainActivity(2);
         settingsScreen.tapSettingsButton();
         // Βεβαιωνόμαστε ότι είμαστε στο Settings root
